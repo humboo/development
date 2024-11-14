3b413bfae16660caf9afd71d035dfe50a51b0084
davidchang613 <davidchang613@gmail.com>
2024-11-14 00:52:55 -0500

    added @PropertySource with spring.profiles.active in application.properties

--- java/README.md
+++ java/README.md
@@ -1,1 +1,2 @@
 Spring Initializr - Java 17 with Maven 3.3.5. com.spring.sample, Lombok and Spring Web dependencies
+added @PropertySource with spring.profiles.active in application.properties
--- java/sample/src/main/java/com/spring/sample/SampleApplication.java
+++ java/sample/src/main/java/com/spring/sample/SampleApplication.java
@@ -2,11 +2,20 @@ package com.spring.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
+
+@PropertySource("classpath:/static/profiles/application-${spring.profiles.active}.properties")
 public class SampleApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(SampleApplication.class); 
+	
 	public static void main(String[] args) {
+		log.info("Starting sample application");
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
